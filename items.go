dfd0f7f0f7a065b43b9b00b30ff703e3a0260518
Sergio Tapia <sergiotapia@outlook.com>
2014-07-10 23:26:07 -0400

    Add api endpoint for getitems func.

--- items.go
+++ items.go
@@ -1,16 +1,47 @@
 package smitego
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+// Item is a struct that represents an Item in Smite.
 type Item struct {
-	Childitemid     int             `json:"ChildItemId"`
-	Devicename      string          `json:"DeviceName"`
-	Iconid          int             `json:"IconId"`
-	ItemDescription ItemDescription `json:"itemDescription"`
-	Itemid          int             `json:"ItemId"`
-	Itemtier        int             `json:"ItemTier"`
-	Price           int             `json:"Price"`
-	Rootitemid      int             `json:"RootItemId"`
-	Shortdesc       string          `json:"ShortDesc"`
-	Startingitem    bool            `json:"StartingItem"`
-	Type            string          `json:"Type"`
-	RetMsg          string          `json:"ret_msg"`
+	ChilditemID      int             `json:"ChildItemId"`
+	DeviceName       string          `json:"DeviceName"`
+	IconID           int             `json:"IconId"`
+	ItemDescription  ItemDescription `json:"itemDescription"`
+	ItemID           int             `json:"ItemId"`
+	ItemTier         int             `json:"ItemTier"`
+	Price            int             `json:"Price"`
+	RootItemID       int             `json:"RootItemId"`
+	ShortDescription string          `json:"ShortDesc"`
+	StartingItem     bool            `json:"StartingItem"`
+	Type             string          `json:"Type"`
+	RetMsg           string          `json:"ret_msg"`
+}
+
+// GetItems returns a new Item array instance with all the Items and their
+// information from Smite.
+func GetItems() []Item {
+	hash := getMD5Hash(DevID + "getitems" + AuthKey + getTimestamp())
+	url := "http://api.smitegame.com/smiteapi.svc/getitemsJson/" + DevID + "/" + hash + "/" + SessionID + "/" + getTimestamp() + "/1"
+
+	response, err := http.Get(url)
+	if err != nil {
+		perror(err)
+	} else {
+		defer response.Body.Close()
+		contents, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			perror(err)
+		} else {
+			var items []Item
+			json.Unmarshal(contents, &items)
+			return items
+		}
+	}
+	items := []Item{}
+	return items
 }
--- smitego.go
+++ smitego.go
@@ -91,29 +91,6 @@ func Ping() string {
 	return "Ping service not available."
 }
 
-func GetItems() []Item {
-	timestamp := time.Now().UTC().Format("20060102150405")
-	hash := getMD5Hash(DevID + "getitems" + AuthKey + timestamp)
-	url := "http://api.smitegame.com/smiteapi.svc/getitemsJson/" + DevID + "/" + hash + "/" + SessionID + "/" + timestamp + "/1"
-
-	response, err := http.Get(url)
-	if err != nil {
-		perror(err)
-	} else {
-		defer response.Body.Close()
-		contents, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			perror(err)
-		} else {
-			var items []Item
-			json.Unmarshal(contents, &items)
-			return items
-		}
-	}
-	items := []Item{}
-	return items
-}
-
 // TODO: Create struct and find out why Request Error is
 // happening on correct API call.
 func GetLeagueSeasons(queueId int) string {
